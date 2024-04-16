796619548766555a345f73da7efd13523ff1bd7f
Yanin <syanin@inbox.ru>
2024-04-16 12:54:37 +0700

    Исправление

--- Test GIT/Program.cs
+++ Test GIT/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine(" А это ебучая ветка Мастера, которую я щас пущу по другому направлению, нежели custom1");
+Console.WriteLine("ИСПРАВЛЕНИЕ ИСПРАВЛЕНИЕ ИСПРАВЛЕНИЕ");
