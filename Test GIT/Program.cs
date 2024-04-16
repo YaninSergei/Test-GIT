9568a955a676153226fc8bef00bab86c9e94876d
Yanin <syanin@inbox.ru>
2024-04-16 23:48:05 +0700

    Комит перед Пушем

--- Test GIT/Program.cs
+++ Test GIT/Program.cs
@@ -1,9 +1,2 @@
 ﻿
-Console.WriteLine("Hello, World!");
-
-<<<<<<< HEAD
-Console.WriteLine(" А это ебучая ветка Мастера, которую я щас пущу по другому направлению, нежели custom1");
-Console.WriteLine("ИСПРАВЛЕНИЕ ИСПРАВЛЕНИЕ ИСПРАВЛЕНИЕ");
-=======
-Console.WriteLine("Заебался я уже делать эти комммиты, 4-я попытка!");
->>>>>>> custom1
+Console.WriteLine("Это пуш основной ветки на гитхаб");
